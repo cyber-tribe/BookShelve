64a68b48aeb5e58545a1206865a8015e8998afd8
cyber-tribe <cybertribe.nks8@gmail.com>
2020-06-06 16:50:06 +0900

    finish js

---
+++ sample.js
@@ -0,0 +1,6 @@
+window.onload=()=>{
+    const prof = document.getElementById("suzuki");
+    prof.addEventListener('mouseover',()=>{
+            prof.src="https://www.u-presscenter.jp/assets_c/2017/09/12423-thumb-226x226-31624.jpg";
+    });
+}
